2cb62d341fcd7d490b05f5ce00c57f11ae1afe9c
DMU-CTEC2902-2021 <usama.mannai@dmu.ac.uk>
2021-03-09 14:53:21 +0000

    HomeContrller return string

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Testing";
         }
 
         public ActionResult About()
